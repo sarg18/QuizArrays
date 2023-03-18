7466ba7598c5d5af23e4a1c9f4ef541706c0a8c5
SERGIO ANDRES RAMIREZ GARCES <sergioramirez117075@correo.itm.edu.co>
2023-03-17 19:42:08 -0500

    Add project files.

---
+++ QuizArray.Logic/OneDimension.cs
@@ -0,0 +1,56 @@
+﻿namespace QuizArray.Logic
+{
+    public class OneDimension
+    {
+        private int[] _array;
+        private int _top;
+
+
+        public OneDimension(int n)
+        {
+            N = n;    
+            _array = new int[n];
+            _top = 0;
+        }
+
+        public bool IsFull => _top == N;
+
+        public bool IsEmpty => _top == 0;
+
+        public int N { get; }
+
+        public void Add(int number)
+        {
+            if (IsFull)
+            {
+                throw new Exception("The array is full.");
+            }
+
+            _array[_top] = number;
+            _top++;
+        }
+
+        public override string ToString()
+        {
+            if (IsEmpty)
+            {
+                return "Empty array.";
+            }
+
+            string output = string.Empty;
+            int count = 0;
+            for (int i = 0; i < _top; i++)
+            {
+                output += $"{_array[i]}\t";
+                count++;
+                if (count > 9)
+                {
+                    count = 0;
+                    output += "\n";
+                }
+            }
+            return output;
+        }
+
+    }
+}
---
+++ QuizArray.Logic/TwoDimensions.cs
@@ -0,0 +1,66 @@
+﻿namespace QuizArray.Logic
+{
+    public class TwoDimensions
+    {
+
+        private int[,] _array;
+      
+        public TwoDimensions(int _m, int _n)
+        {
+            M = _m;
+            N = _n;
+            _array = new int[_m, _n];
+        }
+
+        public int N { get; }
+        public int M { get; }
+
+
+
+        public override string ToString()
+        {
+            var output = string.Empty;
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    output += $"{_array[i, j]}\t";
+                }
+                output += "\n";
+            }
+            return output;
+        }
+
+
+        public void Fill()
+        {
+            Fill(1, 100);
+        }
+
+        public void Fill(int min, int max)
+        {
+            var random = new Random();
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    _array[i, j] = random.Next(min, max);
+                }
+            }
+        }
+
+        public string ToOneDimension()
+        {
+            var output = string.Empty;
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    output += $"{_array[i, j]}\t";
+                }
+            }
+            return output;
+        }
+
+    }   
+}
